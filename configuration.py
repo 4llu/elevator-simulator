d04e2ecc4ccb8b14979c073a30cb063aa2605f80
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2019-04-15 00:17:52 +0800

    Change number of elevators to 3

--- configuration.py
+++ configuration.py
@@ -8,7 +8,7 @@ BUILDING_FULLNESS = 0.8
 # Elevators
 ELEVATOR_AI = "BASIC" # Other options [MEDIUM, ADVANCED]
 CAMERAS = False
-ELEVATOR_NUM = 4
+ELEVATOR_NUM = 3
 ELEVATOR_CAPACITY = 8
 TIME_TO_ELEVATOR = 8 # Ticks it takes to walk to the elevator from the camera in ticks
 ELEVATOR_LOAD_TIME = 4 # Ticks it takes for people to get in and out of the elevator
